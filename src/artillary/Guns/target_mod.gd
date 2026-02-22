f586476c4f2eb827d8cae7129d3fc7df2a8aaa0c
samcspohn <samuel.c.spohn@gmail.com>
2026-02-21 21:53:47 -0600

    fix target mod not persisting changes between reset dynamic/static mod calls

--- src/artillary/Guns/target_mod.gd
+++ src/artillary/Guns/target_mod.gd
@@ -1,6 +1,6 @@
 extends Moddable
 class_name TargetMod
 
-var h_grouping: float = 1.0
-var v_grouping: float = 1.0
-var base_spread: float = 1.0
+@export_storage var h_grouping: float = 1.0
+@export_storage var v_grouping: float = 1.0
+@export_storage var base_spread: float = 1.0
